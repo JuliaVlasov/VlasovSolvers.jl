08b405ee092c130b06d3bb0245aba8940c7f9c85
Pierre Navaro <pnavaro@users.noreply.github.com>
2018-10-27 21:57:39 +0200

    Update and rename VlasovSolvers.jl.jl to VlasovSolvers.jl

---
+++ src/VlasovSolvers.jl
@@ -0,0 +1,11 @@
+module VlasovSolvers.jl
+
+using Reexport
+
+  @reexport using VlasovBase
+  @reexport using SemiLagrangian
+  @reexport using Fourier
+  @reexport using SplittingOperators
+  @reexport using VlasovExamples
+
+end
--- src/VlasovSolvers.jl.jl
+++
@@ -1,5 +0,0 @@
-module VlasovSolvers.jl
-
-greet() = print("Hello World!")
-
-end # module
