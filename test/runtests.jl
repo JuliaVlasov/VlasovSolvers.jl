09869e4c2b6a0b140fedc2b19d912d24ab52c9c3
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2018-10-28 18:44:36 +0100

    remove REQUIRE file

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,13 @@
 #!/usr/bin/env julia
 
+using Pkg
+
+Pkg.add("https://github.com/JuliaVlasov/VlasovBase.jl")
+Pkg.add("https://github.com/JuliaVlasov/SemiLagrangian.jl")
+Pkg.add("https://github.com/JuliaVlasov/Fourier.jl")
+Pkg.add("https://github.com/JuliaVlasov/SplittingOperators.jl")
+Pkg.add("https://github.com/JuliaVlasov/VlasovExamples.jl")
+
 using VlasovSolvers, Test
 
 @test true
