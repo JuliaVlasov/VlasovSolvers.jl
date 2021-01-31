f60a0cde9c253bb605e5848c6f94e763876f91a2
Pierre Navaro <navaro@math.cnrs.fr>
2021-01-31 14:56:04 +0100

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,29 +1,6 @@
 using Test
 using VlasovSolvers
 
-@testset "Landau 1D1V" begin
+include("vlasov-poisson-bsl-spline-1d1v.jl")
+include("vlasov-ampere-fft-1d1v.jl")
 
-dev = CPU()                  # device
-nx, nv = 64, 64              # grid resolution
-stepper = StrangSplitting()  # timestepper
-dt = 0.01                    # timestep
-nsteps = 10                  # total number of time-steps
-
-xmin, xmax = 0, 4π           # X Domain length (m)
-vmin, vmax = -6, 6           # V Domain length (m)
-α  = 0.5                     # Perturbation amplitude
-kx = 0.5                     # Wave number of perturbation
-
-xgrid = OneDGrid(dev, nx, xmin, xmax)
-vgrid = OneDGrid(dev, nv, vmin, vmax)
-
-f = DistributionFunction( xgrid, vgrid )
-
-landau!(f, α, kx)
-
-prob = VlasovProblem(f, BSL(5), dev)
-
-solve!(prob, stepper, dt, nsteps )
-@test true
-
-end
