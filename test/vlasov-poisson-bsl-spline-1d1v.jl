cc3b5eea80f9635d497be7b0c4aa7ab12cb18763
Pierre Navaro <navaro@math.cnrs.fr>
2021-01-31 14:56:08 +0100

    Create vlasov-poisson-bsl-spline-1d1v.jl

---
+++ test/vlasov-poisson-bsl-spline-1d1v.jl
@@ -0,0 +1,27 @@
+@testset "Vlasov-Poisson Landau 1D1V" begin
+
+dev = CPU()                  # device
+nx, nv = 64, 64              # grid resolution
+stepper = StrangSplitting()  # timestepper
+dt = 0.01                    # timestep
+nsteps = 10                  # total number of time-steps
+
+xmin, xmax = 0, 4π           # X Domain length (m)
+vmin, vmax = -6, 6           # V Domain length (m)
+α  = 0.5                     # Perturbation amplitude
+kx = 0.5                     # Wave number of perturbation
+
+xgrid = OneDGrid(dev, nx, xmin, xmax)
+vgrid = OneDGrid(dev, nv, vmin, vmax)
+
+f = DistributionFunction( xgrid, vgrid )
+
+landau!(f, α, kx)
+
+prob = VlasovProblem(f, BSL(5), dev)
+
+sol = solve!(prob, stepper, dt, nsteps )
+
+@test true
+
+end
